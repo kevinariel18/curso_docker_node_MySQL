d902adbff27f9b0cf4be7aca3b28844274576ad1
kevinariel18 <kevincata2005@gmail.com>
2025-10-06 10:34:59 -0500

    nuevo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const jwSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
+console.log('clave secreta: ', jwSecret);
+console.log('Puerto del servidor: ', port);
 
 app.get('/', (req, res) =>{
     res.send('API BACKEND FUNCIONANDO');
 
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(' SERVIDOR ESCUCHANDO EN EL PUERTO ${PORT}');
+app.listen(port, () => {
+    console.log(' SERVIDOR ESCUCHANDO EN EL PUERTO ', port);
 })
